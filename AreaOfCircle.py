1001bd302943359c6681748acd28639fb6dcd9d1
Arnav Dixit <arnavdixit127@gmail.com>
2020-07-19 17:48:49 +0530

    Added comments

--- AreaOfCircle.py
+++ AreaOfCircle.py
@@ -1,3 +1,3 @@
-from math import pi        
-r = float(input("Input the radius of the circle : "))      
-print("The area of the circle with radius",r,"is: {:.2f}".format(pi*r*r))  
+from math import pi       #Importing pi from math 
+r = float(input("Input the radius of the circle : "))      #Taking input from the user
+print("The area of the circle with radius",r,"is: {:.2f}".format(pi*r*r))  #Printing the area to the screen
