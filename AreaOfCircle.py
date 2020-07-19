caedd71d143807b86969e3415e48eb29ae3abfa5
Arnav Dixit <arnavdixit127@gmail.com>
2020-07-19 17:45:09 +0530

    Limited output to 2 decimal places

--- AreaOfCircle.py
+++ AreaOfCircle.py
@@ -1,3 +1,3 @@
-import math
-r = float(input(("Input the radius of the circle : ")))
-print("The area of the circle with radius",r,"is:",math.pi*r*r)
+from math import pi        
+r = float(input("Input the radius of the circle : "))      
+print("The area of the circle with radius",r,"is: {:.2f}".format(pi*r*r))  
